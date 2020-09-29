f90342ffde7a4456234a9d36f938f48a3a56fe66
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2020-09-29 12:51:34 -0400

    Add GotLeaderLatchResource JAX-RS resource class (#14)
    Fixes #6

---
+++ src/main/java/org/kiwiproject/curator/leader/resource/GotLeaderLatchResource.java
@@ -0,0 +1,31 @@
+package org.kiwiproject.curator.leader.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * JAX-RS resource providing a single endpoint that allows clients to determine whether this service
+ * participates in a leader latch.
+ * <p>
+ * Simply make a {@code GET /kiwi/got-leader-latch?} request. The question mark is clearly not required but
+ * is perhaps a little more fun depending on your sense of humor, i.e. if you think programmer humor is funny
+ * or terrible.
+ * <p>
+ * This resource should, of course, only be registered when the service participates in a leader latch.
+ */
+@Path("/kiwi/got-leader-latch")
+@Produces(MediaType.APPLICATION_JSON)
+public class GotLeaderLatchResource {
+
+    /**
+     * Returns a 204 (No Content) response if available. Otherwise, a 404 is returned (though obviously not
+     * by this resource).
+     */
+    @GET
+    public Response gotLeaderLatch() {
+        return Response.noContent().build();
+    }
+}
---
+++ src/test/java/org/kiwiproject/curator/leader/resource/GotLeaderLatchResourceTest.java
@@ -0,0 +1,24 @@
+package org.kiwiproject.curator.leader.resource;
+
+import static org.kiwiproject.test.jaxrs.JaxrsTestHelper.assertNoContentResponse;
+
+import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
+import io.dropwizard.testing.junit5.ResourceExtension;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(DropwizardExtensionsSupport.class)
+@DisplayName("GotLeaderLatchResource")
+class GotLeaderLatchResourceTest {
+
+    private static final ResourceExtension RESOURCE = ResourceExtension.builder()
+            .addResource(new GotLeaderLatchResource())
+            .build();
+
+    @Test
+    void shouldReturn_204_NoContent() {
+        var response = RESOURCE.client().target("/kiwi/got-leader-latch?").request().get();
+        assertNoContentResponse(response);
+    }
+}
