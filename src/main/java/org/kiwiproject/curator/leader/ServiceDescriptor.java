47275ca9e019d63e7abb663672c51fd37aa4d37c
Scott Leberknight <scottleber@gmail.com>
2020-09-29 22:40:30 -0400

    Add class-level javadoc to ServiceDescriptor

--- src/main/java/org/kiwiproject/curator/leader/ServiceDescriptor.java
+++ src/main/java/org/kiwiproject/curator/leader/ServiceDescriptor.java
@@ -3,6 +3,9 @@ package org.kiwiproject.curator.leader;
 import lombok.Builder;
 import lombok.Value;
 
+/**
+ * Value class that contains metadata about a service that participates in a leader latch.
+ */
 @Value
 @Builder
 public class ServiceDescriptor {
