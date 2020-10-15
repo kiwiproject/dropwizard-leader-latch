3715b5b16e801de506ebda39be307480e9c2f9ce
Scott Leberknight <scottleber@gmail.com>
2020-10-14 21:58:23 -0400

    Retrofit with StandardExceptionTests from kiwi-test
    * Remove StandardExceptionTests, replacing it with the one migrated
      into kiwi-test 0.6.0
    
    Fixes #19

--- src/test/java/org/kiwiproject/curator/leader/exception/ManagedLeaderLatchExceptionTest.java
+++ src/test/java/org/kiwiproject/curator/leader/exception/ManagedLeaderLatchExceptionTest.java
@@ -1,6 +1,6 @@
 package org.kiwiproject.curator.leader.exception;
 
-import static org.kiwiproject.curator.leader.test.util.StandardExceptionTests.standardConstructorTestsFor;
+import static org.kiwiproject.test.junit.jupiter.StandardExceptionTests.standardConstructorTestsFor;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicTest;
